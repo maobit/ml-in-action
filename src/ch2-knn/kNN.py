a1a40611700fc329a5f61f770bef01e6e00ef0db
echo42 <qhe028@qq.com>
2017-10-22 18:55:06 +0800

    knn

---
+++ src/ch2-knn/kNN.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from numpy import *
+import operator
+
+
+def createDataset():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
