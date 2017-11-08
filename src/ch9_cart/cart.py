e7ad6d8629a1dbc2190a234a7d85b68ab400f4b7
qheuestc <qheuestc@gmail.com>
2017-11-08 09:39:16 +0800

    cart debugged

--- src/ch9_cart/cart.py
+++ src/ch9_cart/cart.py
@@ -23,9 +23,9 @@ def load_data(file_name):
 
 
 def bin_split_data(data_set, feat_dim, value):
-    mat_right = data_set[np.nonzero(data_set[:, feat_dim] > value)[0], :]
-    mat_left = data_set[np.nonzero(data_set[:, feat_dim] <= value)[0], :]
-    return mat_right, mat_left
+    mat_left = data_set[np.nonzero(data_set[:, feat_dim] > value)[0], :]
+    mat_right = data_set[np.nonzero(data_set[:, feat_dim] <= value)[0], :]
+    return mat_left, mat_right
 
 
 def reg_leaf(data_set):
@@ -60,19 +60,19 @@ def choose_best_split(data_set, leaf_type=reg_leaf, err_type=reg_err, ops=(1, 4)
     best_dim = 0
     best_val = 0
     for feat_dim in range(n - 1):
-        for split_val in set(data_set[:, feat_dim].A.tolist()[0]):
-            mat0, mat1 = bin_split_data(data_set, feat_dim, split_val)
-            if np.shape(mat0)[0] < tol_n or np.shape(mat1)[0] < tol_n:
+        for split_val in set(data_set[:, feat_dim].T.A.tolist()[0]):
+            mat_left, mat_right = bin_split_data(data_set, feat_dim, split_val)
+            if np.shape(mat_left)[0] < tol_n or np.shape(mat_right)[0] < tol_n:
                 continue
-            new_s = err_type(mat0) + err_type(mat1)
+            new_s = err_type(mat_left) + err_type(mat_right)
             if new_s < best_s:
                 best_dim = feat_dim
                 best_val = split_val
                 best_s = new_s
     if (s - best_s) < tol_s:
         return None, leaf_type(data_set)
-    mat0, mat1 = bin_split_data(data_set, best_dim, best_val)
-    if np.shape(mat0)[0] < tol_n or np.shape(mat1)[0] < tol_n:
+    mat_left, mat_right = bin_split_data(data_set, best_dim, best_val)
+    if np.shape(mat_left)[0] < tol_n or np.shape(mat_right)[0] < tol_n:
         return None, leaf_type(data_set)
     return best_dim, best_val
 
